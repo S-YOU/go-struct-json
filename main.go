11270b7d6a659e885132a1fa8897c34770cfb976
S-YOU <S-YOU@users.noreply.github.com>
2022-02-02 04:10:42 +0900

    update

--- main.go
+++ main.go
@@ -20,34 +20,14 @@ import (
 
 	"github.com/iancoleman/strcase"
 	"github.com/jinzhu/inflection"
-	"github.com/k0kubun/pp"
 	"github.com/knq/snaker"
 )
 
 var (
-	out  = flag.String("o", "", "output file")
-	kind = flag.String("kind", "go", "override kind")
+	out      = flag.String("o", "", "output file")
+	kind     = flag.String("kind", "go", "override kind")
+	tagRegex = regexp.MustCompile(`(\w+):"((?:\\.|[^"\\]+)+)"`)
 )
-
-type Field struct {
-	GoName     string   `json:"Name"`
-	GoVarName  string   `json:"name"`
-	NameJson   string   `json:"nameJson"`
-	NameDb     string   `json:"nameDb"`
-	NamesDb    string   `json:"namesDb"`
-	GoType     string   `json:"Type"`
-	GoBaseType string   `json:"baseType"`
-	IsArray    bool     `json:"isArray"`
-	NotNull    bool     `json:"notNull"`
-	Tag        string   `json:"tag,omitempty"`
-	TagFaker   string   `json:"tagFaker,omitempty"`
-	TagSpanner string   `json:"tagSpanner,omitempty"`
-	TagFixture string   `json:"tagFixture,omitempty"`
-	TagGql     string   `json:"tagGql,omitempty"`
-	Docs       []string `json:"docs,omitempty"`
-	Comments   []string `json:"comments,omitempty"`
-	Key        string   `json:"key"`
-}
 
 type Struct struct {
 	GoName      string   `json:"Name"`
@@ -60,9 +40,42 @@ type Struct struct {
 	GoVarNames  string   `json:"names"`
 	Docs        []string `json:"docs,omitempty"`
 	Comments    []string `json:"comments,omitempty"`
-	Fields      []Field  `json:"fields"`
+	Fields      []*Field `json:"fields"`
 	Embeds      []Field  `json:"-"`
+	TypeKind    string   `json:"typeKind"`
 	Key         string   `json:"key"`
+}
+
+type Field struct {
+	GoName     string   `json:"Name"`
+	GoVarName  string   `json:"name,omitempty"`
+	NameJson   string   `json:"nameJson,omitempty"`
+	NameDb     string   `json:"nameDb,omitempty"`
+	NamesDb    string   `json:"namesDb,omitempty"`
+	GoType     string   `json:"Type"`
+	GoBaseType string   `json:"baseType,omitempty"`
+	IsArray    bool     `json:"isArray"`
+	NotNull    bool     `json:"notNull"`
+	Key        string   `json:"key"`
+	Docs       []string `json:"docs,omitempty"`
+	Comments   []string `json:"comments,omitempty"`
+	// struct field
+	Tag        map[string]string `json:"tag,omitempty"`
+	TagFaker   string            `json:"tagFaker,omitempty"`
+	TagSpanner string            `json:"tagSpanner,omitempty"`
+	TagFixture string            `json:"tagFixture,omitempty"`
+	TagGql     string            `json:"tagGql,omitempty"`
+	TypeKind   string            `json:"typeKind"`
+	// intf func
+	Arguments []*Argument `json:"args,omitempty"`
+	Results   []*Argument `json:"results,omitempty"`
+}
+
+type Argument struct {
+	GoType     string `json:"Type"`
+	GoBaseType string `json:"baseType"`
+	IsArray    bool   `json:"isArray"`
+	NotNull    bool   `json:"notNull"`
 }
 
 func main() {
@@ -144,11 +157,153 @@ func parseFile(p string) []*Struct {
 		case *ast.GenDecl:
 			genDecl := node.(*ast.GenDecl)
 			for _, spec := range genDecl.Specs {
-				switch spec.(type) {
+				switch typeSpec := spec.(type) {
 				case *ast.TypeSpec:
-					typeSpec := spec.(*ast.TypeSpec)
+					switch typ := typeSpec.Type.(type) {
+					case *ast.InterfaceType:
+						typeName := typeSpec.Name.Name
+						singularName := inflection.Singular(typeName)
+						pluralName := plural(singularName)
+						goName := snaker.ForceCamelIdentifier(singularName)
+						nameDb := snaker.CamelToSnake(inflection.Singular(goName))
+						st := &Struct{
+							TypeKind:    "interface",
+							GoName:      goName,
+							GoVarName:   typeName,
+							GoNames:     snaker.ForceCamelIdentifier(pluralName),
+							GoVarNames:  strcase.ToLowerCamel(pluralName),
+							NameDb:      nameDb,
+							NamesDb:     plural(nameDb),
+							NameJson:    jsonName(typeName),
+							GoShortName: shortName(goName),
+							Key:         typeName,
+						}
+						if genDecl.Doc != nil {
+							st.Docs = make([]string, len(genDecl.Doc.List))
+							for i, comment := range genDecl.Doc.List {
+								st.Docs[i] = comment.Text
+							}
+						}
+						if typeSpec.Comment != nil {
+							st.Comments = make([]string, len(typeSpec.Comment.List))
+							for i, comment := range typeSpec.Comment.List {
+								st.Comments[i] = comment.Text
+							}
+						}
+						for _, field := range typ.Methods.List {
+							comments := []string(nil)
+							if field.Comment != nil {
+								for _, c := range field.Comment.List {
+									comments = append(comments, c.Text)
+								}
+							}
+							docs := []string(nil)
+							if field.Doc != nil {
+								for _, c := range field.Doc.List {
+									docs = append(docs, c.Text)
+								}
+							}
+							kind := "interface"
+							if _, ok := field.Type.(*ast.FuncType); ok {
+								kind = "func"
+							}
+							fieldType := getType(field.Type)
+							isArray, notNull := getIsArrayAndNotNull(field.Type)
+							baseType := fieldType
+							if isArray {
+								baseType = fieldType[2:]
+							}
+							if len(field.Names) == 0 {
+								st.Fields = append(st.Fields, &Field{
+									TypeKind:   kind,
+									GoType:     fieldType,
+									GoBaseType: baseType,
+									IsArray:    isArray,
+									NotNull:    notNull,
+									Docs:       docs,
+									Comments:   comments,
+									Key:        fieldType,
+								})
+							} else {
+								for _, name := range field.Names {
+									nameJson := jsonName(name.Name)
+									nameDb := snaker.CamelToSnake(inflection.Singular(name.Name))
+									var args, results []*Argument
+									if funcType, ok := field.Type.(*ast.FuncType); ok {
+										for _, x := range funcType.Params.List {
+											fieldType := getType(x.Type)
+											isArray, notNull := getIsArrayAndNotNull(x.Type)
+											baseType := fieldType
+											if isArray {
+												baseType = fieldType[2:]
+											}
+											if len(x.Names) == 0 {
+												args = append(args, &Argument{
+													GoType:     fieldType,
+													GoBaseType: baseType,
+													IsArray:    isArray,
+													NotNull:    notNull,
+												})
+											} else {
+												for range x.Names {
+													args = append(args, &Argument{
+														GoType:     fieldType,
+														GoBaseType: baseType,
+														IsArray:    isArray,
+														NotNull:    notNull,
+													})
+												}
+											}
+										}
+										for _, x := range funcType.Results.List {
+											fieldType := getType(x.Type)
+											isArray, notNull := getIsArrayAndNotNull(x.Type)
+											baseType := fieldType
+											if isArray {
+												baseType = fieldType[2:]
+											}
+											if len(x.Names) == 0 {
+												results = append(results, &Argument{
+													GoType:     fieldType,
+													GoBaseType: baseType,
+													IsArray:    isArray,
+													NotNull:    notNull,
+												})
+											} else {
+												for range x.Names {
+													results = append(results, &Argument{
+														GoType:     fieldType,
+														GoBaseType: baseType,
+														IsArray:    isArray,
+														NotNull:    notNull,
+													})
+												}
+											}
+										}
+									}
+									f := &Field{
+										TypeKind:   kind,
+										GoName:     name.Name,
+										GoVarName:  lowerCamel(name.Name),
+										NameDb:     nameDb,
+										NamesDb:    plural(nameDb),
+										NameJson:   nameJson,
+										GoType:     fieldType,
+										GoBaseType: baseType,
+										IsArray:    isArray,
+										NotNull:    notNull,
+										Docs:       docs,
+										Comments:   comments,
+										Key:        nameJson,
+										Arguments:  args,
+										Results:    results,
+									}
+									st.Fields = append(st.Fields, f)
 
-					switch typeSpec.Type.(type) {
+								}
+							}
+						}
+						structs = append(structs, st)
 					case *ast.StructType:
 						typeName := typeSpec.Name.Name
 						singularName := inflection.Singular(typeName)
@@ -156,6 +311,7 @@ func parseFile(p string) []*Struct {
 						goName := snaker.ForceCamelIdentifier(singularName)
 						nameDb := snaker.CamelToSnake(inflection.Singular(goName))
 						st := &Struct{
+							TypeKind:    "struct",
 							GoName:      goName,
 							GoVarName:   typeName,
 							GoNames:     snaker.ForceCamelIdentifier(pluralName),
@@ -178,15 +334,18 @@ func parseFile(p string) []*Struct {
 								st.Comments[i] = comment.Text
 							}
 						}
-						structType := typeSpec.Type.(*ast.StructType)
-						for _, field := range structType.Fields.List {
-							tag, tagFaker, tagFixture, tagSpanner, tagGql := "", "", "", "", ""
+						for _, field := range typ.Fields.List {
+							tagFaker, tagFixture, tagSpanner, tagGql := "", "", "", ""
+							tag := make(map[string]string, 0)
 							if field.Tag != nil {
-								tag, err = strconv.Unquote(field.Tag.Value)
+								ftag, err := strconv.Unquote(field.Tag.Value)
 								if err != nil {
 									panic(err)
 								}
-								v := reflect.StructTag(tag)
+								v := reflect.StructTag(ftag)
+								for _, x := range tagRegex.FindAllStringSubmatch(string(v), -1) {
+									tag[x[1]] = x[2]
+								}
 								tagFaker = v.Get("faker")
 								tagFixture = v.Get("fixture")
 								if strings.HasPrefix(tagFixture, "string:") {
@@ -211,10 +370,11 @@ func parseFile(p string) []*Struct {
 							isArray, notNull := getIsArrayAndNotNull(field.Type)
 							baseType := fieldType
 							if isArray {
-								fieldType = "[]" + fieldType
+								baseType = fieldType[2:]
 							}
 							if len(field.Names) == 0 {
-								st.Fields = append(st.Fields, Field{
+								st.Fields = append(st.Fields, &Field{
+									TypeKind:   "struct",
 									GoType:     fieldType,
 									GoBaseType: baseType,
 									IsArray:    isArray,
@@ -232,7 +392,8 @@ func parseFile(p string) []*Struct {
 								for _, name := range field.Names {
 									nameJson := jsonName(name.Name)
 									nameDb := snaker.CamelToSnake(inflection.Singular(name.Name))
-									st.Fields = append(st.Fields, Field{
+									st.Fields = append(st.Fields, &Field{
+										TypeKind:   "struct",
 										GoName:     name.Name,
 										GoVarName:  lowerCamel(name.Name),
 										NameDb:     nameDb,
@@ -291,10 +452,11 @@ func getType(field ast.Expr) string {
 	case *ast.StarExpr:
 		return fmt.Sprintf("*%s", getType(typ.X))
 	case *ast.ArrayType:
-		return fmt.Sprintf("%s", getType(typ.Elt))
+		return fmt.Sprintf("[]%s", getType(typ.Elt))
+	case *ast.MapType:
+		return fmt.Sprintf("map[%s]%s", getType(typ.Key), getType(typ.Value))
 	default:
-		_, _ = pp.Println("unknown getType", typ)
-		return "---"
+		return ""
 	}
 }
 
